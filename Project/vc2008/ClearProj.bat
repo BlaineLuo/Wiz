b1f7b3130319c11a1888f9f578a40e8bdbaf926b
BlaineLuo <bbafro@gmail.com>
2016-08-29 17:40:10 +0800

    Remove project settings.

--- Project/vc2008/ClearProj.bat
+++
@@ -1,7 +0,0 @@
-
-DEL /F /Q /S *.aps *.ncb *.user
-
-for /r %1 %%R in (Debug) do if exist "%%R" (rd /s /q "%%R")
-for /r %1 %%R in (Release) do if exist "%%R" (rd /s /q "%%R")
-for /r %1 %%R in (ReleaseCE) do if exist "%%R" (rd /s /q "%%R")
-for /r %1 %%R in (ReleaseXP) do if exist "%%R" (rd /s /q "%%R")
